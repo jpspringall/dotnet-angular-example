5dcf9ea44b92a6e3d6bd546be5c56142cb9ba125
Tom Stones <tom@tomstones.com.au>
2018-01-17 15:31:18 +1100

    Added basic form functionality

--- ClientApp/app/components/customer-add/customer-add.component.ts
+++ ClientApp/app/components/customer-add/customer-add.component.ts
@@ -1,12 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { CustomersService } from '../../services/customers.service';
+import { Validators, AbstractControl, ValidationErrors, FormGroup, FormControl } from '@angular/forms';
 @Component({
     selector: 'customer-add',
     templateUrl: 'customer-add.component.html'
 })
 
 export class CustomerAddComponent implements OnInit {
+    public customerForm: FormGroup;
+    public nameControl: FormControl;
+    public balanceControl: FormControl;
     constructor(private service: CustomersService) { }
 
-    ngOnInit() { }
+    ngOnInit() { 
+        this.nameControl  = new FormControl('', [ Validators.required]);
+        this.balanceControl  = new FormControl('', [Validators.minLength(2), Validators.required]);
+        const fields = {name: this.nameControl, balance: this.balanceControl };
+        this.customerForm = new FormGroup(fields);
+    }
+
+    public handleSubmit(){
+        console.log(this.customerForm.valid);
+    }
 }
