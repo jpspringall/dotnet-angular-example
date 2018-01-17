0a2d20814867cb77f38ad991709a27abc1a0653a
Tom Stones <tom@tomstones.com.au>
2018-01-17 16:30:05 +1100

    Added service intergration

--- ClientApp/app/components/customer-add/customer-add.component.ts
+++ ClientApp/app/components/customer-add/customer-add.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CustomersService } from '../../services/customers.service';
 import { Validators, AbstractControl, ValidationErrors, FormGroup, FormControl } from '@angular/forms';
 import { Customer } from '../../customer';
+import { Subscription } from 'rxjs/Subscription';
+import { Router } from '@angular/router';
 @Component({
     selector: 'customer-add',
     templateUrl: 'customer-add.component.html'
@@ -11,18 +13,24 @@ export class CustomerAddComponent implements OnInit {
     public customerForm: FormGroup;
     public nameControl: FormControl;
     public balanceControl: FormControl;
-    constructor(private service: CustomersService) { }
+    constructor(private service: CustomersService, private router: Router) { }
 
     ngOnInit() { 
-        this.nameControl  = new FormControl('', [ Validators.required]);
-        this.balanceControl  = new FormControl('', [Validators.minLength(2), Validators.required]);
+        this.nameControl  = new FormControl('12312312', [ Validators.required]);
+        this.balanceControl  = new FormControl('123123', [Validators.minLength(2), Validators.required]);
         const fields = {name: this.nameControl, balance: this.balanceControl };
         this.customerForm = new FormGroup(fields);
     }
 
     public handleSubmit(){
         if(this.customerForm.valid){
-            this.service.addCustomer$(this.customer);
+            this.service.addCustomer$(this.customer)
+            .subscribe((response)=>{ 
+                if(response.status === 200){
+                 return this.router.navigate(['/customers'])
+                }
+                console.error('There was an error')
+            });
         }
     }
 
--- ClientApp/app/services/customers.service.ts
+++ ClientApp/app/services/customers.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
-
+import {of} from 'rxjs/observable/of'
 import {Customer} from '../customer';
 
 @Injectable()
@@ -15,7 +15,7 @@ export class CustomersService {
             .map((response: Response) => response.json())
     }
 
-    public addCustomer$(customer: Customer) : Observable<Response>{
-        return this.http.post('/api/Customer', customer)
+    public addCustomer$(customer: any){
+      return  this.http.post('/api/Customer', customer, {headers: new Headers({'content-type': 'application/json'})})
     }
 }
