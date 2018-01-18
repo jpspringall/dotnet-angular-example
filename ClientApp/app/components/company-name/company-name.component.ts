38602fe3ce5f82febfb38aa3df961bee31908e4f
Tom Stones <tom@tomstones.com.au>
2018-01-18 13:32:10 +1100

    Changed company name to come from the initial state, not server

--- ClientApp/app/components/company-name/company-name.component.ts
+++ ClientApp/app/components/company-name/company-name.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-
-import { CompanyNameService } from '../../services/company-name.service'
+import {Store} from '@ngrx/store';
+import { AppModel } from '../../store/models/app.model';
 
 @Component({
     selector: 'company-name',
@@ -10,13 +10,12 @@ import { CompanyNameService } from '../../services/company-name.service'
 })
 
 export class CompanyNameComponent implements OnInit {
-    public name$: Observable<Response>;
+    public name$: Observable<string>;
     public currentDate: Date;
-    constructor(private service: CompanyNameService) { }
+    constructor( private store:Store<AppModel>) { }
 
     ngOnInit() { 
-        this.name$ = this.service.getCompanyName$()
-            .map((res: Response )=> res.json())
+        this.name$ = this.store.select((state)=>state.Company.Name);
         this.currentDate = new Date;
     }
 }
